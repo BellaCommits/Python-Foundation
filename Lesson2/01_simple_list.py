3900773f0803492530e314d60f65843b7d0e08e3
Bella <jacobsbulelwa61@gmail.com>
2026-06-02 21:39:24 +0200

    updated files

---
+++ Lesson2/01_simple_list.py
@@ -0,0 +1,14 @@
+numbers = [10, 20, 30]
+# index starts at 0
+
+print(numbers)
+print(type(numbers))
+
+print()
+
+#Access index 0
+print(numbers[0])
+print(numbers[1])
+print(numbers[5]) #if you try to access something is does not have, you will get an IndexError
+
+
