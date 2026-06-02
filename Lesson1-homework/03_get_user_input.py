ebb444eb670af781de0b7fc1ed508bff65bf6cb3
DevBella <153865442+BellaCommits@users.noreply.github.com>
2026-06-02 21:16:52 +0200

    Add files via upload

---
+++ Lesson1-homework/03_get_user_input.py
@@ -0,0 +1,10 @@
+name = input('Enter name')
+
+print(name)
+print(type(name))
+
+age = input('Enter age')
+age = int(age)
+
+print(age)
+print(type(age))
---
+++ Lesson1-homework/homework.txt.py
@@ -0,0 +1,23 @@
+# Capture information of 2 users. This information includes their name, age and height (in cm).
+# Display each user's information in a sentence format.
+# Then calculate and display the total combined height of both users.
+name = input('Enter name')
+age = input('Enter age')
+height = input('Enter height')
+print()
+
+print(f'You are{name}. You are{age} years old. Your height is{height} in cm.')
+print()
+
+name = input('Enter name')
+age = input('Enter age')
+height = input('Enter height')
+print()
+
+print(f'You are{name}. You are{age} years old. Your height is{height} in cm.')
+print()
+
+age1 = 18
+age2 = 26
+print()
+print(f'Add: {age1 + age2}')
